6fa06c4090d86d3563d41b2f67ed1980280f5e73
Joelios <joel.mesmer@libracore.com>
2019-10-18 15:07:54 +0200

    implemented #1

---
+++ surgery/surgery/doctype/surgery_body_part/test_surgery_body_part.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2019, tüit GmbH and Contributors
+# See license.txt
+from __future__ import unicode_literals
+
+# import frappe
+import unittest
+
+class TestSurgeryBodyPart(unittest.TestCase):
+	pass
--- surgery/surgery/doctype/surgery_report/surgery_report.js
+++ surgery/surgery/doctype/surgery_report/surgery_report.js
@@ -2,7 +2,15 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Surgery Report', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+		// filter for diagnosis based on side data- and surgery body part link-field
+		cur_frm.fields_dict['diagnosis'].get_query = function(doc) {
+			 return {
+				 filters: {
+					 "side": cur_frm.doc.side,
+					 "body_part": cur_frm.doc.body_part
+				 }
+			 }
+		}
+	}
 });
